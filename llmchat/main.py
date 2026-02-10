3127b2832e307ac5e9150ee0d7acd69c3ec19f4d
basil-ajlouney <basil.ajloune24+github@gmail.com>
2026-02-10 03:31:09 +0300

    simple cli-based chat bot that always you to communicate with your bots using ollama models

---
+++ llmchat/main.py
@@ -0,0 +1,84 @@
+import ollama
+import argparse
+import sys
+
+supported_colors = {
+    "black": 30,
+    "red": 31,
+    "green": 32,
+    "yellow": 33,
+    "blue": 34,
+    "magenta": 35,
+    "cyan": 36,
+    "white": 37,
+}
+
+def validate_colors(color):
+    if color not in supported_colors.keys():
+      raise ValueError(f"Unsupported color: {color}")
+
+def validate_model(model):
+  if model not in [model.model for model in ollama.list().models]:
+      raise ValueError(f"Unsupported model: {model}")
+
+def color_text(text: str, color: str) -> str:
+
+  return f"\033[{supported_colors[color]}m{text}\033[0m"
+
+
+def add_message(lst:list, message:str, role:str) -> None:
+  lst.append({"role":role, "content":message})
+
+def clear_input_line():
+    sys.stdout.write("\r\033[2K")
+    sys.stdout.flush()
+
+def main():
+
+  parser = argparse.ArgumentParser(description="arguments for cmd-based chatbot")
+  parser.add_argument("-m", "--model", type=str, default="llama3.1:latest", help="name the model you want to use, defualt: llama3.1:latest")
+  parser.add_argument("-c", "--color", type=str, default="green", help="pick wish color you want to you, check --list-colors for supported colors, default: green")
+  parser.add_argument("-s", "--system", type=str, default="You are a concise assistant.", help="system prompt for the model, default: You are a concise assistant")
+  parser.add_argument("--list-models",action="store_true",default=False, help="list all available models")
+  parser.add_argument("--list-colors",action="store_true",default=False, help="list all available colors")
+
+  args = parser.parse_args()
+
+  if args.list_colors:
+    for color in supported_colors.keys():
+      print(color)
+    return
+
+  if args.list_models:
+    for model in ollama.list().models:
+      print(model.model)
+    return
+
+  validate_colors(args.color)
+  validate_model(args.model)
+
+  messages = [
+        {"role": "system", "content": args.system},
+        # {"role": "user", "content": "Explain transformers in one word."}
+        # {"role": "assistant", "content": "attention."}
+    ]
+
+  while True:
+    try:
+      user_message = input("> ")
+      clear_input_line()
+    except KeyboardInterrupt as e:
+      clear_input_line()
+      print("Ending chat...")
+      return
+    if user_message == "exit":
+      break
+
+    add_message(messages, user_message, "user")
+    answer = ollama.chat(model=args.model, messages=messages)
+
+    print(color_text(answer.message.content, args.color))
+
+    add_message(messages, answer.message.content, answer.message.role)
+if __name__ == "__main__":
+  main()
